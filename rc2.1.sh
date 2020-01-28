852424f7890c311081e21c158caee1b360718463
Derrick Schultz <dvsmethid@gmail.com>
2020-01-27 23:04:35 -0500

    stylegan2

--- rc2.1.sh
+++ rc2.1.sh
@@ -35,22 +35,20 @@ pip install --user torch torchvision
 pip install --user dominate tqdm
 
 #stylegan
-git clone https://github.com/dvschultz/stylegan
+git clone https://github.com/dvschultz/stylegan2
 cd stylegan
-pip install --user -r requirements.txt
 mkdir pkl
 cd pkl
-gdown --id 1JLqXE5bGZnlu2BkbLPD5_ZxoO3Nz-AvF --output inception_v3_features.pkl #inception: https://drive.google.com/open?id=1JLqXE5bGZnlu2BkbLPD5_ZxoO3Nz-AvF
-gdown --id 1gbxwfHNOaGjGsLNTmmSrNA85X2VWHHOq --output vgg16_zhang_perceptual.pkl #zhang: https://drive.google.com/open?id=1gbxwfHNOaGjGsLNTmmSrNA85X2VWHHOq
+gdown --id 1JLqXE5bGZnlu2BkbLPD5_ZxoO3Nz-AvF #inception: https://drive.google.com/open?id=1JLqXE5bGZnlu2BkbLPD5_ZxoO3Nz-AvF
 cd ../
-mkdir pretrained_models
-cd pretrained_models
-gdown --id 1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ --output karras2019stylegan-ffhq-1024x1024.pkl
+mkdir networks
+cd networks
+gdown --id 1UlDmJVLLnBD9SnLSMXeiZRO6g-OMQCA_
 cd ../
 mkdir datasets
 
-#run test
-python pretrained_example.py
+#run a test
+python run_generator.py generate-images --network=./networks/stylegan2-ffhq-config-f.pkl --seeds=6600-6625 --truncation-psi=0.5
 cd ../
 
 cd neural-style-tf
