786c1e427f827ed6db29835152653e31d739b907
Derrick Schultz <dvsmethid@gmail.com>
2020-05-09 19:42:49 -0400

    add opensimplex

--- rc2.1.sh
+++ rc2.1.sh
@@ -39,6 +39,7 @@ git clone https://github.com/dvschultz/stylegan2
 cd stylegan2
 pip uninstall tensorflow
 pip install tensorflow==1.15
+pip install opensimplex
 mkdir pkl
 cd pkl
 gdown --id 1JLqXE5bGZnlu2BkbLPD5_ZxoO3Nz-AvF #inception: https://drive.google.com/open?id=1JLqXE5bGZnlu2BkbLPD5_ZxoO3Nz-AvF
